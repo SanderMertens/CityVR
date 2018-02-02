a071748847631fc1cd76aa43ff5e669d4890e16d
Sander Mertens <sander.mertens8@gmail.com>
2018-02-02 01:10:05 -0800

    Fix center of city, add more variation in tall buildings

--- src/CityVR.c
+++ src/CityVR.c
@@ -1,7 +1,7 @@
 #include <include/CityVR.h>
 
-#define GRID_MAX_X 30
-#define GRID_MAX_Y 30
+#define GRID_MAX_X 40
+#define GRID_MAX_Y 40
 #define MIN_HEIGHT 0.1
 
 float rnd() { return (float)rand() / (float)RAND_MAX; }
@@ -16,19 +16,21 @@ int cortomain(int argc, char *argv[]) {
         for (y = 0; y < GRID_MAX_Y; y ++) {
             float x_c = X_CTR - x, y_c = Y_CTR - y;
             float distance = sqrt((x_c * x_c) + (y_c * y_c)) / MAX_D;
-            float height = 1.0 - distance + rnd() * 0.4 + MIN_HEIGHT;
+            float height = 1.0 - distance + rnd() * 0.7 + MIN_HEIGHT;
 
             if (distance > 0.5)
                 height *= 0.5;
-            else if (rnd() > 0.7)
+            else if (rnd() > 0.9)
                 if (distance < 0.1)
                     height *= 5;
+                else if (distance < 0.2)
+                    height *= 3;
                 else
                     height *= 2;
 
             Building b = corto_declare(data_o, NULL, Building_o);
-            b->x = x - 5;
-            b->y = y - 5;
+            b->x = x - X_CTR;
+            b->y = y - Y_CTR;
             b->width = 0.7;
             b->height = height;
             b->depth = 0.7;
